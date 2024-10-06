f3b204d508cc7da957f0cf621aa1d967847c80ae
thalialevy <thalialevy@hotmail.com>
2024-10-06 12:42:00 -0300

    ultimos ajustes

--- src/main/java/doafacil/entities/User.java
+++ src/main/java/doafacil/entities/User.java
@@ -6,14 +6,7 @@ import java.util.List;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tb_users")
